6f94f1f7c3f74948e8d8d862611390aa8a0d9d02
caizhe666 <cz2513881812@outlook.com>
2021-02-16 13:16:39 +0800

    new!

---
+++ CSoDController/Includes.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include <iostream>
+#include <Windows.h>
+#include "../CSoD/ControlCode.h"
+using namespace std;
---
+++ CSoDController/Main.cpp
@@ -0,0 +1,24 @@
+#include "Includes.hpp"
+
+INT
+wmain(
+	IN PWCHAR wargv[],
+	IN UINT wargc
+)
+{
+	HANDLE hDevice = CreateFileW(L"\\\\.\\CSoD", GENERIC_ALL, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hDevice != INVALID_HANDLE_VALUE)
+	{
+		CSoD_DATA Data = { 0 };
+		Data.BackColor = BV_COLOR_MAGENTA;
+		Data.TextColor = BV_COLOR_RED;
+		strcpy_s(Data.Text, "Hello World!");
+
+		DWORD dwReturned = 0;
+		BOOL bRet = DeviceIoControl(hDevice, CTL_DO_CSoD, &Data, sizeof(Data), NULL, 0, &dwReturned, NULL);
+		if (bRet)
+			;
+	}
+
+	return GetLastError();
+}
